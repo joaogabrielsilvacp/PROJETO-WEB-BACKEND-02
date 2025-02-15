7676197cb2a5d0ce808d59863365f8ab76e948bd
JOAO GABRIEL <joaogabrielsilva@alunos.utfpr.edu.br>
2025-02-14 23:16:35 -0300

    Modelo de dados para os usuários, representando informações como nome, e-mail, senha e permissões (admin).

--- src/models/user.js
+++ src/models/user.js
@@ -16,6 +16,7 @@ const UserSchema = new mongoose.Schema({
 });
 
 
+
 UserSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
     const salt = await bcrypt.genSalt(10);
